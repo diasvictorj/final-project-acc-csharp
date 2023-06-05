19ac273f6a930834634d31796a834c361f12515d
Luiz Ricardo <luizricardo41@gmail.com>
2023-06-05 20:15:31 -0300

    Configuração de injeção de dependencia no Program.cs e modificação na serialização de dados

--- src/Tryitter/Program.cs
+++ src/Tryitter/Program.cs
@@ -2,10 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => 
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TryitterContext>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
